45fd19f5c50239357370eb856526f4786402abf0
Matthew Oballe <oballematt@gmail.com>
2020-11-30 16:11:49 -0600

    creating basic signup page

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Signup from "./components/signup"
+import { Container} from "react-bootstrap"
+
+
+function App() {
+
+ 
+  return (
+    <Container className="d-flex align-items-center justify-content-center"
+    style={{minHeight: "100vh"}}>
+      <div className="w-100" style={{maxWidth: "400px"}}>
+        <Signup/>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
