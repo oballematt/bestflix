9fe6f95b605b98d03d13e7b2c3fc34f23e6d51ad
Matthew Oballe <oballematt@gmail.com>
2020-11-30 17:43:07 -0600

    created routes with basic homepage and login. Added signup page to routes as well

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from "react"
 import Signup from "./components/signup"
-import { Container} from "react-bootstrap"
+import Home from"./components/home"
+import Login from "./components/login"
+import { Container } from "react-bootstrap"
 import { AuthProvider } from "./contexts/AuthContext"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 
 function App() {
 
- 
+
   return (
-    <AuthProvider>
-    <Container className="d-flex align-items-center justify-content-center"
-    style={{minHeight: "100vh"}}>
-      <div className="w-100" style={{maxWidth: "400px"}}>
-        <Signup/>
-      </div>
-    </Container>
-    </AuthProvider>
+    
+      <Container className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route exact path ="/" component={Home}/>
+                <Route path="/signup" component={Signup}/>
+                <Route path="/login" component={Login}/>
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
+  
   );
 }
 
