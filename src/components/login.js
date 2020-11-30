bcac721339c531ef0c5d4ef9d55a88408474cb44
Matthew Oballe <oballematt@gmail.com>
2020-11-30 17:47:05 -0600

    created log in page

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Form, Card, Button, Alert } from "react-bootstrap"
+import { useAuth } from "../contexts/AuthContext"
 
 export default function Login() {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const { signup } = useAuth()
+    const [error, setError] =  useState("")
+    const [loading, setLoading] =  useState(false)
+
+    async function handleSumbit(e){
+        e.preventDefault()
+
+        try {
+          setError("")
+          setLoading(true)
+          await signup(emailRef.current.value, passwordRef.current.value)
+        } catch{
+            setError("Failed to create an account")
+        }
+
+        setLoading(false)
+    }
+
     return (
-        <div>
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Log In</h2>
             
-        </div>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={handleSumbit}>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" ref={emailRef} required></Form.Control>
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} required></Form.Control>
+                        </Form.Group>
+                        <Button disabled={loading} className="w-100" type="submit">Log In</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+               Need an Account? Sign Up
+            </div>
+        </>
     )
 }
