c058631fc3c6863a6257d0067e7ee0e13b869dc0
Matthew Oballe <oballematt@gmail.com>
2020-11-30 18:50:38 -0600

    putting homepage together

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react"
 import Signup from "./components/signup"
 import Home from"./components/home"
 import Login from "./components/login"
-import { Container } from "react-bootstrap"
 import { AuthProvider } from "./contexts/AuthContext"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
@@ -11,10 +10,6 @@ function App() {
 
 
   return (
-    
-      <Container className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}>
-        <div className="w-100" style={{ maxWidth: "400px" }}>
           <Router>
             <AuthProvider>
               <Switch>
@@ -24,8 +19,6 @@ function App() {
               </Switch>
             </AuthProvider>
           </Router>
-        </div>
-      </Container>
   
   );
 }
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react'
-import { Form, Card, Button, Alert } from "react-bootstrap"
+import { Form, Card, Button, Alert, Container } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
 
@@ -31,7 +31,9 @@ export default function Signup() {
     }
 
     return (
-        <>
+        <Container className="d-flex align-items-center justify-content-center"
+            style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
             <Card>
                 <Card.Body>
                     <h2 className="text-center mb-4">Sign Up</h2>
@@ -57,6 +59,7 @@ export default function Signup() {
             <div className="w-100 text-center mt-2">
                 Already have an account? <Link to="/login">Log in</Link>
             </div>
-        </>
+        </div>
+        </Container>
     )
 }
